47c5f1945d4a087da08478065321ebeb9abc5a71
danil <danil.bruhanov@gmail.com>
2018-09-11 01:48:30 +0300

    add chart

--- app/src/main/java/com/task/test/bdswiss/MainActivity.kt
+++ app/src/main/java/com/task/test/bdswiss/MainActivity.kt
@@ -3,12 +3,19 @@ package com.task.test.bdswiss
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.github.mikephil.charting.data.Entry
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.TimeUnit
+import android.R.attr.entries
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.data.LineData
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +24,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     var disposable: Disposable? = null
+    val entries = arrayListOf<Entry>()
+    var time = 0f
+    val dataSet = LineDataSet(entries, "Label")
+
+    val lineData = LineData(dataSet)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,13 +41,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun getRates() {
         disposable =
-                
-
-        Observable.interval( 2, TimeUnit.SECONDS)
+        Observable.interval( 10, TimeUnit.SECONDS)
                 .flatMap { bdService.getRates() }
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         { result ->
+                            time += 10
+                            entries.add(Entry(time, result.rates.get(0).price.toFloat()))
+
+                            
+
+                            chart.data = lineData
+                            dataSet.notifyDataSetChanged()
+                            lineData.notifyDataChanged()
+                            chart.notifyDataSetChanged()
+                            chart.invalidate()
+
                             ratesText.text = result.toString()
                         },
                         { error -> Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show() }
