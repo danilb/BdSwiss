77847874d8b6dea47fb5d2a1a26d2a42cf820808
danil <danil.bruhanov@gmail.com>
2018-09-11 01:01:11 +0300

    get rates repeatedly

--- app/src/main/java/com/task/test/bdswiss/BDService.kt
+++ app/src/main/java/com/task/test/bdswiss/BDService.kt
@@ -1,4 +1,25 @@
 package com.task.test.bdswiss
 
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
 interface BDService {
+    @GET("rates")
+    fun getRates(): Observable<RatesModel>
+
+    companion object {
+        fun create(): BDService {
+            val retrofit = Retrofit.Builder()
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl("https://mt4-api-staging.herokuapp.com")
+                    .build()
+
+            return retrofit.create(BDService::class.java)
+        }
+    }
 }
+
--- app/src/main/java/com/task/test/bdswiss/MainActivity.kt
+++ app/src/main/java/com/task/test/bdswiss/MainActivity.kt
@@ -2,11 +2,49 @@ package com.task.test.bdswiss
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    val bdService by lazy {
+        BDService.create()
+    }
+
+    var disposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ratesButton.setOnClickListener {
+            getRates()
+        }
+    }
+
+    private fun getRates() {
+        disposable =
+                
+
+        Observable.interval( 2, TimeUnit.SECONDS)
+                .flatMap { bdService.getRates() }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { result ->
+                            ratesText.text = result.toString()
+                        },
+                        { error -> Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show() }
+                )
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disposable?.dispose()
     }
 }
